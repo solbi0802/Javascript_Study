64a2c8f1235017538335299745eff561f797fd0f
DESKTOP-J80L466\Solbi-PC <solbi5555@gmail.com>
2018-08-16 23:29:04 +0900

    ch4-4 (112p)까지

---
+++ ch4/4-25.js
@@ -0,0 +1,8 @@
+ var test = 'This is test';
+ console.log(window.test);
+
+ var sayFoo = function() {
+ 	console.log(this.test);
+ }
+
+ sayFoo();
---
+++ ch4/4-26.js
@@ -0,0 +1,22 @@
+var value = 100;
+
+var myObject = {
+	value : 1,
+	func1: function () {
+		this.value += 1;
+		console.log('func2() called. this.value : ' + this.value);
+
+		func2 = function () {
+			this.value += 1;
+			console.log('func2() called. this.value : ' + this.value);
+	
+			func3 = function() {
+				this.value += 1;
+				console.log('func3() called. this.value : ' + this.value);
+				}
+			func3();
+		}
+		func2();
+	}
+};
+myObject.func1();
